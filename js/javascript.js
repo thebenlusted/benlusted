fbb7e82c9614182cfcd20623a702994cc2f47ab0
Ben Lusted <blustedaudio@gmail.com>
2020-01-12 12:55:05 -0800

    Added visual content and functionality to the work tab
    Nailing the design for the work tab and working on making an accessible template for people to buy and purchase. (adding comments, documentation, etc)

--- js/javascript.js
+++ js/javascript.js
@@ -1,64 +1,130 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.about-text').hide();
   $('.contact-info').hide();
 
-  $('#landing-info').animate({width: "100%"}, 450);
-  $('#my-work-tab').animate({width: "96%"}, 600);
-  $('#about-tab').animate({width: "92%"}, 750);
-  $('#home-tab').animate({width: "88%"}, 900);
+  $('#landing-info').animate({
+    width: "100%"
+  }, 450);
+  $('#my-work-tab').animate({
+    width: "96%"
+  }, 600);
+  $('#about-tab').animate({
+    width: "92%"
+  }, 750);
+  $('#home-tab').animate({
+    width: "88%"
+  }, 900);
 });
 $(function() {
   let active = "slide-3";
-    $('#home-btn').click(function(){
-    $('#home-tab').animate({width: '88%'});
-    $('.landing-info-text h1').animate({left:'0'});
-    $('.landing-info-text h2').animate({left:'0'});
+  $('#home-btn').click(function() {
+    $('#home-tab').animate({
+      width: '88%'
+    });
+    $('.landing-info-text h1').animate({
+      left: '0'
+    });
+    $('.landing-info-text h2').animate({
+      left: '0'
+    });
 
-    $('#about-tab').animate({width:'92%'});
-    $('.about-text').animate({left:'0px'});
+    $('#about-tab').animate({
+      width: '92%'
+    });
+    $('.about-text').animate({
+      left: '0px'
+    });
 
-    $('#my-work-tab').animate({width:'96%'});
+    $('#my-work-tab').animate({
+      width: '96%'
+    });
   });
 
   $('#about-btn').click(function() {
-    $('#home-tab').animate({width: '60'});
-    $('.landing-info-text h1').animate({left:'-1000px'});
-    $('.landing-info-text h2').animate({left:'-1000px'});
+    $('#home-tab').animate({
+      width: '60'
+    });
+    $('.landing-info-text h1').animate({
+      left: '-1000px'
+    });
+    $('.landing-info-text h2').animate({
+      left: '-1000px'
+    });
 
-    $('#about-tab').animate({width:'92%'});
-    $('.about-text').animate({left:'0px'});
+    $('#about-tab').animate({
+      width: '92%'
+    });
+    $('.about-text').animate({
+      left: '0px'
+    });
     $('.about-text').show();
 
-    $('#my-work-tab').animate({width:'96%'});
+    $('#my-work-tab').animate({
+      width: '96%'
+    });
 
 
 
   });
 
   $('#my-work-btn').click(function() {
-    $('#home-tab').animate({width: '60'});
-    $('.landing-info-text h1').animate({left:'-1000px'});
-    $('.landing-info-text h2').animate({left:'-1000px'});
+    $('#home-tab').animate({
+      width: '60'
+    });
+    $('.landing-info-text h1').animate({
+      left: '-1000px'
+    });
+    $('.landing-info-text h2').animate({
+      left: '-1000px'
+    });
 
-    $('#about-tab').animate({width: '120'});
-    $('.about-text').animate({left:'-1500px'});
+    $('#about-tab').animate({
+      width: '120'
+    });
+    $('.about-text').animate({
+      left: '-1500px'
+    });
 
-    $('#my-work-tab').animate({width:'96%'});
+    $('#my-work-tab').animate({
+      width: '96%'
+    });
     $('.my-work-content').show();
-    $('.my-work-content').animate({left:'0px'});
+    $('.my-work-content').animate({
+      left: '0px'
+    });
+
   });
 
   $('#contact-btn').click(function() {
-    $('#home-tab').animate({width:'60'});
-    $('.landing-info-text h1').animate({left:'-1000px'});
-    $('.landing-info-text h2').animate({left:'-1000px'});
+    $('#home-tab').animate({
+      width: '60'
+    });
+    $('.landing-info-text h1').animate({
+      left: '-1000px'
+    });
+    $('.landing-info-text h2').animate({
+      left: '-1000px'
+    });
 
-    $('#my-work-tab').animate({width:'180'});
-    $('.my-work-content').animate({left:'-1500px'});
+    $('#my-work-tab').animate({
+      width: '180'
+    });
+    $('.my-work-content').animate({
+      left: '-1500px'
+    });
 
-    $('#about-tab').animate({width:'120'});
-    $('.about-text').animate({left:'-1500px'});
+    $('#about-tab').animate({
+      width: '120'
+    });
+    $('.about-text').animate({
+      left: '-1500px'
+    });
 
     $('.contact-info').show();
+  });
+
+
+  $('.skill-category-card').mouseenter(function() {
+    
   });
 });
